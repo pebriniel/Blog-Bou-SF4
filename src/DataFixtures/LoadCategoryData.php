5514c9c6ad54721ece4d5d30b384b662c9b30d8b
pebriniel <zeldalyoko@gmail.com>
2018-05-10 10:40:41 +0200

    [ADD:P] Ajout des fixtures

---
+++ src/DataFixtures/LoadCategoryData.php
@@ -0,0 +1,43 @@
+<?php 
+
+//src/DataFixtures/ORM/LoadCategoryData.php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class LoadCategoryData extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $design = new Category();
+        $design->setName('Design');
+        $manager->persist($design);
+
+        $programming = new Category();
+        $programming->setName('Programming');
+        $manager->persist($programming);
+
+        $managing = new Category();
+        $managing->setName('Manager');
+        $manager->persist($managing);
+
+        $administrator = new Category();
+        $administrator->setName('Administrator');
+        $manager->persist($administrator);
+
+        $manager->flush();
+
+        $this->addReference('category-design', $design);
+        $this->addReference('category-programming', $programming);
+        $this->addReference('category-managing', $managing);
+        $this->addReference('category-administrator', $administrator);
+    }
+
+    public function getOrder()
+    {
+        return 1;
+    }
+}
---
+++ src/DataFixtures/LoadUserData.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class LoadUserData extends Fixture
+{
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        foreach ($this->getUserData() as [$fullname, $username, $password, $email, $roles]) {
+            $user = new User();
+            $user->setFullName($fullname);
+            $user->setUsername($username);
+            $user->setPassword($this->passwordEncoder->encodePassword($user, $password));
+            $user->setEmail($email);
+            $user->setRoles($roles);
+
+            $manager->persist($user);
+            $this->addReference($username, $user);
+        }
+
+        $manager->flush();
+    }
+
+    private function getUserData(): array
+    {
+        return [
+            // $userData = [$fullname, $username, $password, $email, $roles]; //ROLE_ADMIN - ROLE_USER
+            ['Test admin', 'admin', 'test', 'admin@test.com', ['ROLE_ADMIN']],
+        ];
+    }
+
+
+}
